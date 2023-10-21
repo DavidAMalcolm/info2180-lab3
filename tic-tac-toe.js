62126eb3c4ce93dc30cadb827075340ff227949a
ChildishWu <david.a.malcolms@gmail.com>
2023-10-21 02:35:41 -0500

    Enabled Winning Feature

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -5,8 +5,28 @@ document.addEventListener("DOMContentLoaded", function() {
 
     var parent = document.getElementById("board");
     let gametrack = [];
+    let token = "cross"
     var child = parent.getElementsByTagName("div");
         
+    function brute(){
+        const winningCombinations = [
+            [0, 1, 2], [3, 4, 5], [6, 7, 8],
+            [0, 3, 6], [1, 4, 7], [2, 5, 8],
+            [0, 4, 8], [2, 4, 6] 
+        ];
+        for (const combo of winningCombinations) {
+            const [a, b, c] = combo;
+            if (
+                gametrack[a] === gametrack[b] &&
+                gametrack[b] === gametrack[c] &&
+                gametrack[a]
+            ) {
+                return true;
+            }
+        }
+        return false; 
+    }
+
     for (let i = 0; i < child.length; i++) {
 
         child[i].classList.add("square");
@@ -20,23 +40,32 @@ document.addEventListener("DOMContentLoaded", function() {
         })
 
         child[i].addEventListener("click",function(){
-            if(gametrack.length == 0){
-                gametrack.push("X")
+            if(gametrack.length == 0 || token === "X"){
+                gametrack[i] = "X"
+                console.log(gametrack)
+                token = "O"
                 child[i].textContent = "X"
                 child[i].classList.remove("O")
                 child[i].classList.add("X")
             }
-            else if (gametrack[gametrack.length-1] == "X"){
-                gametrack.push("O")
+            else if (token == "O"){
+                gametrack[i] = "O"
+                console.log(gametrack)
+                token = "X"
                 child[i].textContent = "O"
                 child[i].classList.remove("X")
                 child[i].classList.add("O")
             }
-            else{
-                gametrack.push("X")
-                child[i].textContent = "X"
-                child[i].classList.remove("O")
-                child[i].classList.add("X")
+            if (brute()){
+                if (token=="X"){
+                    token = "O"
+                }
+                else{
+                    token = "X"
+                }
+                let statusBoard = document.getElementById("status");
+                statusBoard.textContent = "Congragulations! " + token + " is the Winner!";
+                statusBoard.classList.add("you-won");
             }
     })
     
